ba4c072ad26ed3d083de8082fc0eb290ef936d8f
Andrjuha-joma-jozhik <andrey.zhmudenko1@gmail.com>
2023-11-02 16:07:34 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,80 @@
-import Image from 'next/image'
+'use client'
+
+import clsx from 'clsx'
+import React, { ReactNode, useState } from 'react'
+
+type WeatherType = {
+  city: string,
+  temp: number,
+  humidity: number
+  wind: number
+}
+
+const getSearch = async (city: string) => {
+  const res = await fetch(process.env.NEXT_PUBLIC_API_URL + city + process.env.NEXT_PUBLIC_API_KEY, {
+    method: 'GET',
+  })
+
+  return await res.json()
+}
 
 export default function Home() {
+
+  const [city, setCity] = useState<string>('')
+  const [weather, setWeather] = useState<WeatherType | undefined>(undefined)
+  const [error, setError] = useState<string | undefined>(undefined)
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    getSearch(city)
+      .then((res) => {
+        if (res.cod === 200) {
+          setError(undefined)
+          setWeather({
+            city: res.name,
+            temp: res.main.temp,
+            humidity: res.main.humidity,
+            wind: res.wind.speed
+          })
+        }
+        if (res.cod === '404') {
+          setWeather(undefined)
+          setError('Invalid ')
+        }
+      })
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className='flex flex-col gap-2 h-max p-2 rounded text-xl border border-gray-50/50 w-full sm:max-w-[700px]'>
+      <form onSubmit={onSubmit} className='flex gap-2 flex-row w-full'>
+        <input value={city} required type='text' onChange={(e) => setCity(e.target.value)} className='w-full p-1 border-gray-50/50 bg-black border rounded' size={1} />
+        <button type="submit" className='rounded border-gray-50/50 border px-1 hover:text-black hover:bg-white transition-all'>
+          Search
+        </button>
+      </form>
+      {
+        weather ? <Weather weather={weather} /> :
+          error ? <Span type='error'>{error}</Span> : null
+      }
+    </div>
+  )
+}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+const Weather = ({ weather }: { weather: WeatherType }) => {
+  return (
+    <div className='flex flex-col gap-3'>
+      <Span>City: {weather.city}</Span>
+      <Span>Temperature: {weather.temp + '\u00B0'}C</Span>
+      <Span>Humidity: {weather.humidity}%</Span>
+      <Span>Wind Speed: {weather.wind} km/h</Span>
+    </div>
+  )
+}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+const Span = ({ children, type = 'default' }: { children: ReactNode, type?: 'error' | 'default' }) => {
+  return (
+    <span className={clsx('flex justify-start sm:justify-center', type === 'default' && 'border-b border-gray-50/30 pb-1', type === 'error' && 'border-none')}>
+      {children}
+    </span>
   )
 }
